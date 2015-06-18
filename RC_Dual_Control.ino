c480fffe219bd15077249f14d265b73b2cc93c14
Bill Levien <blevien@safaribooksonline.com>
2015-06-18 15:48:25 -0700

    Updated Pins

--- RC_Dual_Control.ino
+++ RC_Dual_Control.ino
@@ -1,6 +1,6 @@
 /*
  Read pulse length from Spektrum 2.4 Ghz Reciever for dual motor control, and spin 2 motors
- L293D Code Adapted from 
+ L293D Code Adapted from
  https://www.safaribooksonline.com/library/view/beginning-arduino/9781430232407/project_16_using_an_l293d_motor_driver_ic.html
 */
 
@@ -10,12 +10,12 @@
 unsigned long steerDuration;
 unsigned long throttleDuration;
 
-#define motorPin1 10 // L293D Input 1
-#define motorPin2 11// L293D Input 2
-#define throttleOutput 12 //L293D Speed Pin
+#define motorPin3 11 // L293D Input 1
+#define motorPin4 12// L293D Input 2
+#define throttleOutput 10 //L293D Speed Pin
 
-#define motorPin3 6 // L293D Input 1
-#define motorPin4 7// L293D Input 2
+#define motorPin1 6 // L293D Input 1
+#define motorPin2 7// L293D Input 2
 #define steeringOutput 5 //L293D Speed Pin
 
 int speed = 0;
@@ -24,48 +24,58 @@ int direction = 0;
 
 void setup()
 {
-  
-  Serial.begin(9600);
+
   pinMode(steeringInput, INPUT);
   pinMode(throttleInput, INPUT);
-  
+
   pinMode(throttleOutput, OUTPUT);
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-  
+
   pinMode(steeringOutput, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
 
+  Serial.begin(9600);
+
 }
 
-void setMotor(int output, int pin1, int pin2, int Mspeed){
+void setMotor(int output, int pin1, int pin2, int Mspeed) {
 
-  analogWrite(output, abs(Mspeed)); // write speed to Enable 1 pin
-  
-  if (Mspeed > 0) { // Rotate motor clockwise
+  if (Mspeed < 20 && Mspeed > -20){
+    analogWrite(output, 0); // write speed to Enable 1 pin
+  }
+  else {
+    analogWrite(output, abs(Mspeed)); // write speed to Enable 1 pin
+  }
+   
+  if (Mspeed > 20) { // Rotate motor clockwise
     digitalWrite(pin1, LOW); // set Input 1 of the L293D low
     digitalWrite(pin2, HIGH); // set Input 2 of the L293D high
+    Serial.println(String(output) + "||" + String(abs(Mspeed)));
   }
-  else {  // Rotate motor anti-clockwise
+  
+  else if (Mspeed < -20) {  // Rotate motor anti-clockwise
     digitalWrite(pin1, HIGH); // set Input 1 of the L293D low
     digitalWrite(pin2, LOW); // set Input 2 of the L293D high
+    Serial.println(String(output) + "||" + String(abs(Mspeed)));
   }
+
   
 }
 
-void loop(){
-  
- steerDuration = pulseIn(steeringInput, HIGH);
- throttleDuration = pulseIn(throttleInput, HIGH);
- 
- Serial.println(String(speed) + "||" + String(direction));
- 
- direction = constrain(map(steerDuration, 1100, 1900, -255, 255), -255, 255);
- speed = constrain(map(throttleDuration, 1100, 1900, -255, 255), -255, 255); 
- 
- setMotor(steeringOutput, motorPin3, motorPin4, direction);
- setMotor(throttleOutput, motorPin1, motorPin2, speed);
+
+
+void loop() {
+
+  steerDuration = pulseIn(steeringInput, HIGH);
+  throttleDuration = pulseIn(throttleInput, HIGH);
+
+  direction = constrain(map(steerDuration, 1100, 1900, -255, 255), -255, 255);
+  speed = constrain(map(throttleDuration, 1100, 1900, -255, 255), -255, 255);
+
+  setMotor(steeringOutput, motorPin1, motorPin2, direction);
+  setMotor(throttleOutput, motorPin3, motorPin4, speed);
 
 }
 
